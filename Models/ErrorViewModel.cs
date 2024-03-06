f100dad9527e19534f2d800e73bec9b610feec78
oleksii kostrikin <oleksii kostrikin@TLM084>
2024-03-06 15:24:23 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebNetAlex.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
